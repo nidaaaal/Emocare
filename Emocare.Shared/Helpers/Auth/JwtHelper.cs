89b993705725c1aa7121d5a9cf65e814afba9fb4
nidal <nidalcheruvadi@gmail.com>
2025-07-16 16:51:08 +0530

    Add project files.

---
+++ Emocare.Shared/Helpers/Auth/JwtHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Emocare.Domain.Entities.Auth;
+using Emocare.Domain.Interfaces.Helper;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace Emocare.Shared.Helpers.Auth
+{
+    public class JwtHelper : IJwtHelper
+    {
+           private readonly IConfiguration _configuration;
+        public JwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        public string GetJwtToken(Users user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]);
+
+            var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, user.FirstName),
+            new(ClaimTypes.Email, user.EmailAddress),
+            new(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(_configuration.GetValue<int>("Jwt:ExpiryInDays")),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+    }
+
